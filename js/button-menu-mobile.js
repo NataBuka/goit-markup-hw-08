67c3425e9c43525140719e7bab628e96c401a082
NataBuka <natabuksha30@gmail.com>
2022-04-14 21:03:57 +0300

    add mobile modal

--- js/button-menu-mobile.js
+++ js/button-menu-mobile.js
@@ -1,10 +1,11 @@
 (() => {
     const btnMenuRef = document.querySelector("[data-button-menu]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
     btnMenuRef.addEventListener("click", () => {
-        //const expanded =
-        //btnMenuRef.getAttribute("aria-expanded") === "true"|| false;
+        const expanded =
+        btnMenuRef.getAttribute("aria-expanded") === "true"|| false;
         btnMenuRef.classList.toggle("is-open");
-        //btnMenuRef.getAttribute("aria-expanded", !expanded);
-        //mobileMenuRef.classList.toggle("is-open");
+        btnMenuRef.getAttribute("aria-expanded", !expanded);
+        mobileMenuRef.classList.toggle("is-open");
     });
 })();
